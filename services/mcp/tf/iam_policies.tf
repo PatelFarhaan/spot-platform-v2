97d7195b973155b6f948901088e1938103fa475d
Farhaan Patel <mpatel@roku.com>
2022-11-23 21:04:45 -0500

    Added AWS Billing dashboard to Grafana and updated IAM Policy

--- services/mcp/tf/iam_policies.tf
+++ services/mcp/tf/iam_policies.tf
@@ -1,38 +1,39 @@
 // SES Policy
 resource "aws_iam_policy" "ec2_read_only" {
   description = "EC2 Read only access"
-  name        = "ec2ro-${local.config_data.name}-${local.config_data.env}"
+  name        = "ec2-ro-${local.config_data.name}-${local.config_data.env}"
   path        = "/${local.config_data.name}/${local.config_data.env}/"
 
   policy = jsonencode({
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Effect": "Allow",
-            "Action": "ec2:Describe*",
-            "Resource": "*"
-        },
-        {
-            "Effect": "Allow",
-            "Action": "elasticloadbalancing:Describe*",
-            "Resource": "*"
-        },
-        {
-            "Effect": "Allow",
-            "Action": [
-                "cloudwatch:ListMetrics",
-                "cloudwatch:GetMetricStatistics",
-                "cloudwatch:Describe*"
-            ],
-            "Resource": "*"
-        },
-        {
-            "Effect": "Allow",
-            "Action": "autoscaling:Describe*",
-            "Resource": "*"
-        }
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Action" : "ec2:Describe*",
+        "Resource" : "*"
+      },
+      {
+        "Effect" : "Allow",
+        "Action" : "elasticloadbalancing:Describe*",
+        "Resource" : "*"
+      },
+      {
+        "Effect" : "Allow",
+        "Action" : [
+          "cloudwatch:Describe*",
+          "cloudwatch:ListMetrics",
+          "cloudwatch:GetMetricData",
+          "cloudwatch:GetMetricStatistics"
+        ],
+        "Resource" : "*"
+      },
+      {
+        "Effect" : "Allow",
+        "Action" : "autoscaling:Describe*",
+        "Resource" : "*"
+      }
     ]
-})
+  })
 
   tags = local.config_data.tags
 }
