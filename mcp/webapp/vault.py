9bf151c5110ada60e04bd81e26b48ef1af2ad2a6
Farhaan Patel <mpatel@roku.com>
2023-01-06 00:08:19 -0500

    Completed 90% integration for mcp

---
+++ mcp/webapp/vault.py
@@ -0,0 +1,134 @@
+import sys
+import hvac
+import json
+import requests
+from os import environ
+from functools import wraps
+
+
+def check_if_service_exists(method):
+    @wraps(method)
+    def _impl(self, *method_args, **method_kwargs):
+        if not self.service_exists():
+            message = "Service does not exist"
+            return self.send_response(result=False, message=message)
+        return method(self, *method_args, **method_kwargs)
+
+    return _impl
+
+
+class Vault:
+    def __init__(self):
+        self.vault_token = None
+        self.service = "backend"
+        self.username = "app_role"
+        self.password = "password"
+        self.application = "webapp"
+        self.mount_point = {"mount_point": "kv"}
+        # self.vault_address = environ["VAULT_ADDR"]
+        self.vault_address = "http://localhost:8200"
+        self.path = f"{self.service}/{self.application}"
+
+        self.client = self.init_server()
+
+    def retrieve_token(self):
+        payload = dict()
+        payload["data"] = json.dumps({"password": self.password})
+        payload["url"] = f"{self.vault_address}/v1/auth/userpass/login/{self.username}"
+
+        _response = requests.post(**payload).json()
+        self.vault_token = _response["auth"]["client_token"]
+
+    @staticmethod
+    def send_response(result: bool, message=None, data=None):
+        return {
+            "data": data,
+            "result": result,
+            "message": message
+        }
+
+    def init_server(self):
+        self.retrieve_token()
+        _client = hvac.Client(
+            url=self.vault_address,
+            token=self.vault_token
+        )
+        if not _client.is_authenticated():
+            print("UNABLE TO CONNECT TO VAULT. PLEASE VALIDATE THE VAULT ADDRESS AND PASSWORD!!!")
+            sys.exit(1)
+        return _client
+
+    def service_exists(self):
+        try:
+            _response = self.client.secrets.kv.v2.list_secrets(self.service, **self.mount_point)
+            if self.application in _response["data"]["keys"]:
+                return True
+        except Exception as e:
+            print(e)
+            return False
+
+    def get_secret_obj(self) -> dict:
+        _response = self.client.secrets.kv.v2.read_secret(path=self.path, **self.mount_point)
+        return _response["data"]["data"]
+
+    def write_secret(self, key, value):
+        if not all([key, value]):
+            message = "None of Key and Vault should be null"
+            return self.send_response(result=False, message=message)
+
+        if not self.service_exists():
+            _secret = {key: value}
+        else:
+            _secret = self.get_secret_obj()
+            _secret.update({key: value})
+
+        _response = self.client.secrets.kv.v2.create_or_update_secret(
+            secret=_secret,
+            path=self.path,
+            **self.mount_point
+        )
+        return self.send_response(result=True, data=_response)
+
+    @check_if_service_exists
+    def read_secret(self, key: str):
+        _secret = self.get_secret_obj()
+        if key in _secret:
+            return self.send_response(result=True, data=_secret[key])
+
+        message = "Secret does not exist"
+        return self.send_response(result=False, message=message)
+
+    @check_if_service_exists
+    def delete_secret(self, key: str) -> dict:
+        _secret = self.get_secret_obj()
+        if key in _secret:
+            _secret.pop(key)
+            self.client.secrets.kv.v2.create_or_update_secret(
+                secret=_secret,
+                path=self.path,
+                **self.mount_point
+            )
+            return self.send_response(result=True, message="Secret Deleted")
+
+        message = "Secret does not exist"
+        return self.send_response(result=False, message=message)
+
+    @check_if_service_exists
+    def list_secrets(self):
+        _secret = self.get_secret_obj()
+        return self.send_response(result=True, data=_secret)
+
+
+if __name__ == '__main__':
+    vault_obj = Vault()
+    response = vault_obj.write_secret("farhaan", "world3asd")
+    print("Write data response: ", response)
+
+    response = vault_obj.read_secret("farhaanopk")
+    print("Read data response: ", response)
+
+    response = vault_obj.delete_secret("farhaan")
+    print("Delete data response: ", response)
+
+    response = vault_obj.list_secrets()
+    print("List Secrets response: ", response)
