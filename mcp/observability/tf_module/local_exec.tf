c6e66f8130d05b121bee75fee9a9b9103a4626b1
Farhaan Patel <mpatel@roku.com>
2023-01-30 15:02:51 -0500

    Updated observability to push sg to cluster_config.json

--- mcp/observability/tf_module/local_exec.tf
+++ mcp/observability/tf_module/local_exec.tf
@@ -4,25 +4,28 @@ resource "null_resource" "push_output_to_s3" {
   }
 
   depends_on = [
-    aws_s3_bucket.mcp_bucket,
-    aws_lb.global_dev_apps_lb,
-    aws_lb.global_mcp_apps_lb,
-    aws_s3_bucket.worker_bucket,
-    aws_s3_bucket.tfstate_bucket,
-    aws_kms_key.vault_auto_unseal,
-    aws_ecr_repository.apps_private_repo,
-    aws_dynamodb_table.tfstate_dynamodb_table,
-    aws_lb_target_group.global_mcp_apps_target_group,
-    aws_security_group.global_dev_apps_lb_security_group,
-    aws_security_group.global_mcp_apps_lb_security_group
+    aws_security_group.app_sg,
+    aws_instance.ec2_instance,
+    aws_route53_record.www_redirect,
+    aws_route53_record.dualstack_alias,
+    aws_lb_target_group.target_group_port_80,
+    aws_lb_target_group.target_group_port_9090,
+    aws_lb_target_group.target_group_port_9093,
+    aws_acm_certificate.mcp_observability_app_certs,
+    aws_route53_record.mcp_observability_acm_records,
+    aws_lb_listener_certificate.grafana_certs_attachment_port_443,
+    aws_lb_listener_certificate.grafana_certs_attachment_port_9090,
+    aws_lb_listener_certificate.grafana_certs_attachment_port_9093
   ]
 
   provisioner "local-exec" {
     command = <<EOF
-
+      mkdir -p /tmp/spotops &&
       mcp_bucket_name=`cat ./../config.yml | yq -r .s3_buckets.mcp` &&
-      terraform output -json | jq .outputs.value > /tmp/cluster_config.json &&
-      aws s3 cp /tmp/cluster_config.json s3://$mcp_bucket_name
+      aws s3 cp s3://$mcp_bucket_name/cluster_config.json /tmp/spotops/base_config.json &&
+      terraform output -json | jq .outputs.value > /tmp/spotops/observability_config.json &&
+      jq -s '.[0] * .[1]' /tmp/spotops/base_config.json /tmp/spotops/observability_config.json > /tmp/spotops/cluster_config.json &&
+      aws s3 cp /tmp/spotops/cluster_config.json s3://$mcp_bucket_name
     EOF
   }
 }
