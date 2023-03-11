c87c78eb0a9912f1adb2846983eae6906f74382c
Farhaan Patel <mpatel@roku.com>
2023-03-10 22:17:29 -0500

    adding asg to mcp generic module

--- mcp/platform/deployment/tf/main.tf
+++ mcp/platform/deployment/tf/main.tf
@@ -23,19 +23,23 @@ locals {
 module "mcp_deployment_stack" {
   source = "../../tf_module"
 
+  ebs_volume_size              = local.config_data.ebs_volume_size
   app                          = local.config_data.app
   env                          = local.config_data.env
   tags                         = local.config_data.tags
   region                       = local.config_data.region
   key_name                     = local.config_data.key_name
   dns_names                    = local.config_data.dns_names
-  instance_type                = local.config_data.instance_type
   kms_id                       = local.cluster_config.vault_kms_id
+  spot_instance_type           = local.config_data.spot_config.instance_type
   zone_name                    = local.cluster_config.global_zone_name_1
   private_key_name_path        = local.config_data.private_key_name_path
   global_mcp_load_balancer_arn = local.cluster_config.global_mcp_apps_lb_arn
   mcp_vault_bucket             = local.cluster_config.s3_mcp_vault_bucket_name
   mcp_spot_bucket              = local.cluster_config.s3_mcp_spot_plane_bucket_name
+  spot_asg_min_instances       = local.config_data.spot_config.auto_scaling_group.min_instances
+  spot_asg_max_instances       = local.config_data.spot_config.auto_scaling_group.max_instances
   name                         = "${local.config_data.app}-${local.config_data.env}"
+  spot_asg_desired_instances   = local.config_data.spot_config.auto_scaling_group.desired_instances
   regional_name                = "${local.config_data.app}-${local.config_data.env}-${local.config_data.region}"
 }
